af5cc8bb8c3834b2765b4f5913b16d173b042bac
SofiaFrg <up1083801@upnet.gr>
2024-03-01 02:49:39 +0200

    final

--- ex0.py
+++ ex0.py
@@ -17,5 +17,16 @@ url = input("Enter the url:")  # προσδιορισμός του url
 if not url.startswith("http://"):
     url='http://'+url
 with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
+    #html = response.text
+    #more(html)
+    print(f"Website headers are {url}\n,{response.headers}\n\n")
+    server=response.headers.get('server')
+    if server:
+        print(f"the server is {server}")
+    else:
+        print("no server found")
+    cookies=response.headers.get('Set-Cookie')
+    if cookies:
+        print(f"the cookies are {cookies} ")
+    else:
+        print("no cookies found")
